b58164c66fcb9d2ab070e786d05c96b3a198af63
Timmatt Lee <timmatt@google.com>
2022-01-10 10:35:00 +0800

    fix: await queryUser

--- app-start/local/index.ts
+++ app-start/local/index.ts
@@ -26,23 +26,106 @@ import IntentFlow = smarthome.IntentFlow;
 const SERVER_PORT = 3388;
 
 interface IWasherParams {
-  on?: boolean,
-  start?: boolean,
-  pause?: boolean,
+  on?: boolean;
+  start?: boolean;
+  pause?: boolean;
 }
 
 class LocalExecutionApp {
+  constructor(private readonly app: App) {}
 
-  constructor(private readonly app: App) { }
+  identifyHandler(
+    request: IntentFlow.IdentifyRequest
+  ): Promise<IntentFlow.IdentifyResponse> {
+    console.log("IDENTIFY intent: " + JSON.stringify(request, null, 2));
 
-  identifyHandler(request: IntentFlow.IdentifyRequest):
-      Promise<IntentFlow.IdentifyResponse> {
-    // TODO: Implement device identification
+    const scanData = request.inputs[0].payload.device.udpScanData;
+    if (!scanData) {
+      const err = new IntentFlow.HandlerError(
+        request.requestId,
+        "invalid_request",
+        "Invalid scan data"
+      );
+      return Promise.reject(err);
+    }
+
+    // In this codelab, the scan data contains only local device id.
+    const localDeviceId = Buffer.from(scanData.data, "hex");
+
+    const response: IntentFlow.IdentifyResponse = {
+      intent: Intents.IDENTIFY,
+      requestId: request.requestId,
+      payload: {
+        device: {
+          id: "washer",
+          verificationId: localDeviceId.toString(),
+        },
+      },
+    };
+    console.log("IDENTIFY response: " + JSON.stringify(response, null, 2));
+
+    return Promise.resolve(response);
   }
 
-  executeHandler(request: IntentFlow.ExecuteRequest):
-      Promise<IntentFlow.ExecuteResponse> {
-    // TODO: Implement local execution
+  executeHandler(
+    request: IntentFlow.ExecuteRequest
+  ): Promise<IntentFlow.ExecuteResponse> {
+    console.log("EXECUTE intent: " + JSON.stringify(request, null, 2));
+
+    const command = request.inputs[0].payload.commands[0];
+    const execution = command.execution[0];
+    const response = new Execute.Response.Builder().setRequestId(
+      request.requestId
+    );
+
+    const promises: Array<Promise<void>> = command.devices.map((device) => {
+      console.log(
+        "Handling EXECUTE intent for device: " + JSON.stringify(device)
+      );
+
+      // Convert execution params to a string for the local device
+      const params = execution.params as IWasherParams;
+      const payload = this.getDataForCommand(execution.command, params);
+
+      // Create a command to send over the local network
+      const radioCommand = new DataFlow.HttpRequestData();
+      radioCommand.requestId = request.requestId;
+      radioCommand.deviceId = device.id;
+      radioCommand.data = JSON.stringify(payload);
+      radioCommand.dataType = "application/json";
+      radioCommand.port = SERVER_PORT;
+      radioCommand.method = Constants.HttpOperation.POST;
+      radioCommand.isSecure = false;
+
+      console.log("Sending request to the smart home device:", payload);
+
+      return this.app
+        .getDeviceManager()
+        .send(radioCommand)
+        .then(() => {
+          const state = { online: true };
+          response.setSuccessState(device.id, Object.assign(state, params));
+          console.log(`Command successfully sent to ${device.id}`);
+        })
+        .catch((e: IntentFlow.HandlerError) => {
+          e.errorCode = e.errorCode || "invalid_request";
+          response.setErrorState(device.id, e.errorCode);
+          console.error("An error occurred sending the command", e.errorCode);
+        });
+    });
+
+    return Promise.all(promises)
+      .then(() => {
+        return response.build();
+      })
+      .catch((e) => {
+        const err = new IntentFlow.HandlerError(
+          request.requestId,
+          "invalid_request",
+          e.message
+        );
+        return Promise.reject(err);
+      });
   }
 
   /**
@@ -50,30 +133,30 @@ class LocalExecutionApp {
    */
   getDataForCommand(command: string, params: IWasherParams): unknown {
     switch (command) {
-      case 'action.devices.commands.OnOff':
+      case "action.devices.commands.OnOff":
         return {
-          on: params.on ? true : false
+          on: params.on ? true : false,
         };
-      case 'action.devices.commands.StartStop':
+      case "action.devices.commands.StartStop":
         return {
-          isRunning: params.start ? true : false
+          isRunning: params.start ? true : false,
         };
-      case 'action.devices.commands.PauseUnpause':
+      case "action.devices.commands.PauseUnpause":
         return {
-          isPaused: params.pause ? true : false
+          isPaused: params.pause ? true : false,
         };
       default:
-        console.error('Unknown command', command);
+        console.error("Unknown command", command);
         return {};
     }
   }
-}
+} 
 
-const localHomeSdk = new App('1.0.0');
+const localHomeSdk = new App("1.0.0");
 const localApp = new LocalExecutionApp(localHomeSdk);
 localHomeSdk
   .onIdentify(localApp.identifyHandler.bind(localApp))
   .onExecute(localApp.executeHandler.bind(localApp))
   .listen()
-  .then(() => console.log('Ready'))
+  .then(() => console.log("Ready"))
   .catch((e: Error) => console.error(e));
